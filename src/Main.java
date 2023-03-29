8e65e8222de5559916005f58a06ae22ec755c5a0
Damian Villalba <damianalumno@gmail.com>
2023-03-28 21:28:22 -0300

    VERSION FINAL AUTOMATIZADO

--- src/Main.java
+++ src/Main.java
@@ -6,48 +6,23 @@ import java.util.List;
 
 public class Main {
     public static void main(String[] args) throws IOException{
+        // args[0] tiene que ser el archivo del pronostico y args[1] tiene que ser el archivo del resultado. Sino hay error.
         //armado de instancias
         List<Pronostico> pronosticos = new ArrayList<>();
-        Equipo equipo1 = new Equipo();
-        Equipo equipo2 = new Equipo();
-        Partido partido = new Partido();
-        Pronostico pronostico = new Pronostico();
         List<Partido> partidos = new ArrayList<>();
-        for(int indice = 1; indice <= cantidadPartidos(args[0]); indice++){
-            List<String> datosPartido = infoArchivo(args[1], indice);
-            equipo1.setNombre(datosPartido.get(0));
-            equipo2.setNombre(datosPartido.get(3));
-            int golesEquipo1 = Integer.parseInt(datosPartido.get(1));
-            int golesEquipo2 = Integer.parseInt(datosPartido.get(2));
-            partido.setEquipo1(equipo1);
-            partido.setEquipo2(equipo2);
-            partido.setGolesEquipo1(golesEquipo1);
-            partido.setGolesEquipo2(golesEquipo2);
-            pronostico.setPartido(partido);
-            pronostico.setResultado(resultadoPronostico(infoArchivo(args[0], indice)));
-            pronosticos.add(pronostico);
-            System.out.println(pronostico.getPartido().getEquipo1().getNombre()); //SALE ARGENTINA PRIMERO Y SEGUNDO POLONIA
-            partidos.add(partido);
-        }
+        armadoDeInstancias(args[0], args[1], pronosticos, partidos);
+        //creo la ronda con los datos ya incorporados
         Ronda ronda1 = new Ronda("1", partidos);
-        //resultado
-        System.out.println("--------------");
-        System.out.println(pronosticos.get(0).getPartido().getEquipo1().getNombre()); //SALE SOLO POLONIA A PESAR DE QUE SALE ARGENTINA EN LA PRIMERA VUELTA
-        System.out.println("--------------");
-        for(Pronostico pronostico1 : pronosticos){
-            System.out.println(pronostico1.getPartido().getEquipo1().getNombre());
-            System.out.println(pronostico1.getResultado());
-        }
-
+        //resultado con los pronosticos de cada partido evaluado
         System.out.println("Puntaje del pronostico: " + ronda1.puntos(pronosticos));
     }
 
-    private static List<String> infoArchivo(String archivoResultado, int partido) throws IOException {
+    private static List<String> infoArchivo(String archivo, int partido) throws IOException {
         //sacamos una lista con la informacion del partido (al ser univocos no importa cual de los dos archivos procesar para sacar los equipos)
         //el parametro partido indica el numero de partido correspondiente (linea), debe existir ese partido sino da error.
         List<String> informacion = null;
         int partidoActual = 1;
-        for (String linea : Files.readAllLines(Paths.get(archivoResultado))){
+        for (String linea : Files.readAllLines(Paths.get(archivo))){
             if(partido != partidoActual){
                 partidoActual++;
                 continue;
@@ -76,5 +51,27 @@ public class Main {
             partidos++;
         }
         return partidos;
+    }
+    private static void armadoDeInstancias(String archivoPronostico, String archivoResultado, List<Pronostico> listaPronosticos, List<Partido> listaPartidos) throws IOException{
+        // creo instancias de pronostico y defino los partidos para cada uno, agregandolos a la lista correspondiente.
+        for(int indice = 1; indice <= cantidadPartidos(archivoPronostico); indice++){
+            Equipo equipo1 = new Equipo();
+            Equipo equipo2 = new Equipo();
+            Partido partido = new Partido();
+            Pronostico pronostico = new Pronostico();
+            List<String> datosPartido = infoArchivo(archivoResultado, indice);
+            equipo1.setNombre(datosPartido.get(0));
+            equipo2.setNombre(datosPartido.get(3));
+            int golesEquipo1 = Integer.parseInt(datosPartido.get(1));
+            int golesEquipo2 = Integer.parseInt(datosPartido.get(2));
+            partido.setEquipo1(equipo1);
+            partido.setEquipo2(equipo2);
+            partido.setGolesEquipo1(golesEquipo1);
+            partido.setGolesEquipo2(golesEquipo2);
+            pronostico.setPartido(partido);
+            pronostico.setResultado(resultadoPronostico(infoArchivo(archivoPronostico, indice)));
+            listaPronosticos.add(pronostico);
+            listaPartidos.add(partido);
+        }
     }
 }
